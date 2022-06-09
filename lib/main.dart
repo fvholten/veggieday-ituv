d8a1c93f4fca8839af594716a9a939dbbf788ece
Frootiloops <timo.p1996@web.de>
2022-06-09 17:47:58 +0200

    auth emulator

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   await FirebaseAuth.instance.signInAnonymously();
   runApp(const MyApp());
 }
